d11ea8afa0e585784c9e938ac97ceed65293f196
NguyenMinh <nguyenminhhodac@gmail.com>
2021-03-30 19:30:59 +0700

    done

--- Buoi4/index.js
+++ Buoi4/index.js
@@ -1,1 +1,9 @@
-document.getElementsByTagName(input)
+let list = document.getElementById('list');
+document.getElementById('form').onsubmit = (e) => {
+  // ngăn chặn load trang khi bấm submit
+  e.preventDefault();
+  let fullname = document.getElementById('fullname').value;
+  let age = document.getElementById('age').value;
+  console.log(fullname+" "+age);
+  list.innerHTML += '<li>' + fullname +" "+age + '</li>';
+}
