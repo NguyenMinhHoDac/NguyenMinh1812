4c022fd7fb87a99f00d419dcd12c1b815bf2fb08
NguyenMinh <nguyenminhhodac@gmail.com>
2021-04-08 17:16:21 +0700

    task5

---
+++ Buoi5/script.js
@@ -0,0 +1,11 @@
+let box = document.getElementById('box');
+let close = document.getElementById('block6');
+function boxchat(){
+    box.style.display="none";
+    close.style.display="flex";
+}
+
+function close(){
+    close.style.display="none";
+    box.style.display="block";
+}
