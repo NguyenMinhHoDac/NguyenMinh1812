b1992f0175c86d819925bb324647c232aa796cec
NguyenMinh <nguyenminhhodac@gmail.com>
2021-04-08 09:39:25 +0700

    new

--- Buoi4/index.js
+++ Buoi4/index.js
@@ -3,5 +3,21 @@ document.getElementById('form').onsubmit = (e) => {
   e.preventDefault();
   let fullname = document.getElementById('fullname').value;
   let age = document.getElementById('age').value;
-  list.innerHTML += '<li>' + fullname +"  "+age + '</li>';
+  let t=kt(fullname,age);
+  if (t===true) {
+    list.innerHTML += '<li>' + fullname +"  "+age + '</li>';
+  } 
+  else
+  {
+    #b1.innerHTML +="Vui long nhap lai";
+  }
+}
+function kt(fullname,age){
+  if (fullname==='') {return false;}
+  for (let i = 0; i < fullname.length; i++) {
+    if (fullname.charCodeAt(i)>=48 && ten.charCodeAt(i)<=57) {return false}
+  }
+  if (age==='') {return false;}
+  if (age<= 0 || age>=100) {return false;}
+  return true;
 }
