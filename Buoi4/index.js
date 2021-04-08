ec9d2c993b61e596537d52c4c6b8ac3e938735d5
NguyenMinh <nguyenminhhodac@gmail.com>
2021-04-08 14:10:08 +0700

    done task 4

--- Buoi4/index.js
+++ Buoi4/index.js
@@ -1,23 +1,32 @@
-let list = document.getElementById('list');
+let list1 = document.getElementById('list1');
+let list2 = document.getElementById('list2');
+let er= document.getElementById('box')
+function kt(fullname,age){
+  if (fullname==='') {return false;}
+  for (let i = 0; i < fullname.length; i++) {
+    if (fullname.charCodeAt(i)>=48 && fullname.charCodeAt(i)<=57) {return false;}
+  }
+  if (age==='') {return false;}
+  if (age<= 0 || age>=100) {return false;}
+  return true;
+}
+
 document.getElementById('form').onsubmit = (e) => {
   e.preventDefault();
   let fullname = document.getElementById('fullname').value;
   let age = document.getElementById('age').value;
   let t=kt(fullname,age);
   if (t===true) {
-    list.innerHTML += '<li>' + fullname +"  "+age + '</li>';
+    list1.innerHTML +=  fullname +'<br>';
+    list2.innerHTML += age +'<br>';
   } 
   else
   {
-    #b1.innerHTML +="Vui long nhap lai";
+    er.style.display="block";
   }
 }
-function kt(fullname,age){
-  if (fullname==='') {return false;}
-  for (let i = 0; i < fullname.length; i++) {
-    if (fullname.charCodeAt(i)>=48 && ten.charCodeAt(i)<=57) {return false}
-  }
-  if (age==='') {return false;}
-  if (age<= 0 || age>=100) {return false;}
-  return true;
+
+function closebox(){
+  er.style.display="none";
 }
+
