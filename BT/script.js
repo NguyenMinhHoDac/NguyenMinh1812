e89c1509b178c70ac27ff8521af963183e43a131
NguyenMinh <nguyenminhhodac@gmail.com>
2021-04-19 19:00:19 +0700

    new

--- BT/script.js
+++ BT/script.js
@@ -11,12 +11,15 @@ var id=0;
 id = sessionStorage.getItem('id');
 function themDuLieu(event){
     event.preventDefault();
-    let name = document.querySelector("#name").value;
+    let name = document.querySelector("#name").value; 
+    name=name.trim();
     let category = document.querySelector("#category").value;
-    let getimg = document.querySelector("#preview-image").src;console.log(name.charCodeAt(0));
+    let getimg = document.querySelector("#preview-image").src;
+    let img = document.getElementById("input-image").value;
     if (name==''||name.length>10||(name.charCodeAt(0)>=48 && name.charCodeAt(0)<=57)){
         document.getElementById("note1").style.display="block";
         document.getElementById("note2").style.display="none";
+        document.getElementById("note3").style.display="none";
     }
     else{
         document.getElementById("note1").style.display="none";
@@ -25,30 +28,41 @@ function themDuLieu(event){
         }
         else{   
             document.getElementById("note2").style.display="none"; 
-            id++;
-            sessionStorage.setItem('id',id);
-            let new_row = 
-                `<tr id="row">
-                    <td id="cell-id">${id}</td>
-                    <td><input type="text" readonly value='${name}' id="cell-name"></td>
-                    <td>        
-                        <select id="cell-category">
-                            <option>${category}</option>
-                        </select>
-                    </td>
-                    <td>
-                        <img src="${getimg}" id="cell-img">
-                        <input type="file" accept="image/*" id="input-newimage">
-                    </td>
-                    <td id="action">
-                        <button onclick="editRow(event)" class="btn btn-outline-primary" id="button-edit">Edit</button>
-                        <button onclick="deleteRow(event)" class="btn btn-outline-danger" id="button-delete">Delete</button>
-                        <button onclick="saveRow(event)" class="btn btn-outline-success" id="button-save">Save</button>
-                        <button onclick="cancelRow(event)" class="btn btn-outline-secondary" id="button-cancel">Cancel</cancel>
-                    </td>
-                </tr>`
-            document.querySelector("tbody").innerHTML += new_row;
-        sessionStorage.setItem(id,[name,category,getimg]);
+            if(img==''||(img.indexOf('.png')==-1 && img.indexOf('.jpg')==-1 && img.indexOf('jpeg')==-1 && img.indexOf('.gif')==-1)){
+                document.getElementById("note3").style.display="block";
+            }
+            else{
+                document.getElementById("note3").style.display="none";
+                id++;
+                sessionStorage.setItem('id',id);
+                let new_row = 
+                    `<tr id="row">
+                        <td id="cell-id">${id}</td>
+                        <td>
+                            <input type="text" readonly value='${name}' id="cell-name">
+                            <p id="note4">Vui lòng nhập lại</p>
+                        </td>
+                        <td>        
+                            <select id="cell-category">
+                                <option>${category}</option>
+                            </select>
+                            <p id="note5">Vui lòng chọn khóa</p>
+                        </td>
+                        <td>
+                            <img src="${getimg}" id="cell-img">
+                            <input type="file" accept="image/*" id="input-newimage">
+                            <p id="note6">Vui lòng chọn ảnh</p>
+                        </td>
+                        <td id="action">
+                            <button onclick="editRow(event)" class="btn btn-outline-primary" id="button-edit">Edit</button>
+                            <button onclick="deleteRow(event)" class="btn btn-outline-danger" id="button-delete">Delete</button>
+                            <button onclick="saveRow(event)" class="btn btn-outline-success" id="button-save">Save</button>
+                            <button onclick="cancelRow(event)" class="btn btn-outline-secondary" id="button-cancel">Cancel</cancel>
+                        </td>
+                    </tr>`
+                document.querySelector("tbody").innerHTML += new_row;
+                sessionStorage.setItem(id,[name,category,getimg]);
+            }
         }
     }
 }
@@ -60,15 +74,20 @@ function layDuLieu(){
         let new_row = 
         `<tr id="row">
             <td id="cell-id">${i}</td>
-            <td><input type="text" readonly value='${data[0]}' id="cell-name"></td>
+            <td>
+                <input type="text" readonly value='${data[0]}' id="cell-name">
+                <p id="note4">Vui lòng nhập lại</p>
+            </td>
             <td>            
                 <select id="cell-category">
                     <option>${data[1]}</option>
                 </select>
+                <p id="note5">Vui lòng chọn khóa</p>
             </td>
             <td>
                 <img src="${data[2]}" id="cell-img">
                 <input type="file" accept="image/*" id="input-newimage">
+                <p id="note6">Vui lòng chọn ảnh</p>
             </td>
             <td id="action">
                 <button onclick="editRow(event)" class="btn btn-outline-primary" id="button-edit">Edit</button>
@@ -83,31 +102,57 @@ function layDuLieu(){
 function editRow(event){
     event.target.parentNode.parentNode.querySelector("input").readOnly = false;
     event.target.parentNode.parentNode.querySelector("#cell-name").style.border="solid 1px";
+    let category = event.target.parentNode.parentNode.querySelector("#cell-category").value;
     event.target.parentNode.parentNode.querySelector("#cell-category").innerHTML="<option>Chọn khóa</option><option>K18</option><option>K19</option><option>K20</option>";
+    event.target.parentNode.parentNode.querySelector("#cell-category").value=category;
     event.target.parentNode.parentNode.querySelector("#input-newimage").style.display="inline-block";
+    
     event.target.parentNode.parentNode.querySelector("#input-newimage").onchange =() =>{
-        let files = event.target.parentNode.parentNode.querySelector("#input-newimage").files;
-        let blob = URL.createObjectURL(files[0]);
-        event.target.parentNode.parentNode.querySelector("#cell-img").src = blob;}
+        let img= event.target.parentNode.parentNode.querySelector("#input-newimage").value;
+        if(img==''||(img.indexOf('.png')==-1 && img.indexOf('.jpg')==-1 && img.indexOf('jpeg')==-1 && img.indexOf('.gif')==-1)){
+            document.getElementById("note6").style.display="block";
+        }
+        else{
+            document.getElementById("note6").style.display="none";
+            let files = event.target.parentNode.parentNode.querySelector("#input-newimage").files;
+            let blob = URL.createObjectURL(files[0]);
+            event.target.parentNode.parentNode.querySelector("#cell-img").src = blob;
+        }
+    }
     event.target.parentNode.parentNode.querySelector("#button-edit").style.display="none";
     event.target.parentNode.parentNode.querySelector("#button-delete").style.display="none";
     event.target.parentNode.parentNode.querySelector("#button-save").style.display="inline-block";
     event.target.parentNode.parentNode.querySelector("#button-cancel").style.display="inline-block";
 }
 function saveRow(event){
-    event.target.parentNode.parentNode.querySelector("input").readOnly = true;
     let id2 = event.target.parentNode.parentNode.querySelector("#cell-id").innerHTML; 
     let name = event.target.parentNode.parentNode.querySelector("#cell-name").value; 
+    name=name.trim();
     let category = event.target.parentNode.parentNode.querySelector("#cell-category").value;
     let getimg = event.target.parentNode.parentNode.querySelector("#cell-img").src;
-    sessionStorage.setItem(id2,[name,category,getimg]);
-    event.target.parentNode.parentNode.querySelector("#cell-category").innerHTML=`<option>${category}</option>`;
-    event.target.parentNode.parentNode.querySelector("#cell-name").style.border="none";
-    event.target.parentNode.parentNode.querySelector("#input-newimage").style.display="none";
-    event.target.parentNode.parentNode.querySelector("#button-edit").style.display="inline-block";
-    event.target.parentNode.parentNode.querySelector("#button-delete").style.display="inline-block";
-    event.target.parentNode.parentNode.querySelector("#button-save").style.display="none";
-    event.target.parentNode.parentNode.querySelector("#button-cancel").style.display="none";
+    if (name==''||name.length>10||(name.charCodeAt(0)>=48 && name.charCodeAt(0)<=57)){
+        document.getElementById("note4").style.display="block";
+        document.getElementById("note5").style.display="none";
+        document.getElementById("note6").style.display="none";
+    }
+    else{
+        document.getElementById("note4").style.display="none";
+        if(category=='Chọn khóa'){
+            document.getElementById("note5").style.display="block";
+        }
+        else{   
+            document.getElementById("note5").style.display="none"; 
+            sessionStorage.setItem(id2,[name,category,getimg]);
+            event.target.parentNode.parentNode.querySelector("input").readOnly = true;
+            event.target.parentNode.parentNode.querySelector("#cell-category").innerHTML=`<option>${category}</option>`;
+            event.target.parentNode.parentNode.querySelector("#cell-name").style.border="none";
+            event.target.parentNode.parentNode.querySelector("#input-newimage").style.display="none";
+            event.target.parentNode.parentNode.querySelector("#button-edit").style.display="inline-block";
+            event.target.parentNode.parentNode.querySelector("#button-delete").style.display="inline-block";
+            event.target.parentNode.parentNode.querySelector("#button-save").style.display="none";
+            event.target.parentNode.parentNode.querySelector("#button-cancel").style.display="none";
+        }
+    }
 }
 function deleteRow(event){
     let i;
