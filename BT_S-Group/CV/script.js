9b67d90d232fa609bbca160e432cc796265d89e6
NguyenMinh <nguyenminhhodac@gmail.com>
2021-05-14 01:15:47 +0700

    task2

---
+++ BT_S-Group/CV/script.js
@@ -0,0 +1,11 @@
+document.getElementById('button3').onclick = () => {
+    document.getElementById('blockbody').style.display="none";
+  }
+
+document.getElementById('button1').onclick = () => {
+    document.getElementById('blockbody').style.display="block";
+  }
+
+document.getElementById('button2').onclick = () => {
+    document.getElementById('blockbody').style.display="block";
+  }
