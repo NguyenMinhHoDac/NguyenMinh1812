63470229683eaf32c486367f31076c223d47e177
NguyenMinh <nguyenminhhodac@gmail.com>
2021-05-18 22:49:22 +0700

    task4

--- BT_S-Group/CV/script.js
+++ BT_S-Group/CV/script.js
@@ -10,8 +10,21 @@ document.getElementById('button2').onclick = () => {
     document.getElementById('blockbody').style.display="block";
   }
 
-function moveTop(){
-    document.getElementById("tab").animate({marginTop: "0px"}, 500);
-    document.getElementById("menu").animate({opacity: 0}, 500);
+function hideMenu() {
+    document.getElementById("tab").classList.add("move1");
+    document.getElementById("menu").classList.add("hide");
+    document.getElementById('blockbody').style.display="block";
+    setTimeout(function(){document.getElementById('menu').style.display="none"},500);
 }
 
+function openMenu() {
+  document.getElementById('menu').style.display="flex";
+  document.getElementById("menu").classList.remove("hide");
+  document.getElementById("tab").classList.remove("move1");
+}
+
+document.getElementById("bar").onclick = () =>{
+  let kt=document.getElementById('menu').style.display;
+  if (kt=="flex") {hideMenu()}
+  else {openMenu()};
+}
