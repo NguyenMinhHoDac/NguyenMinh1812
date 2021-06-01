f150ffdef43eee465e17875d4e447ef101547676
NguyenMinhHoDac <nguyenminhhodac@gmail.com>
2021-06-01 17:51:37 +0700

    blog

--- BT_S-Group/CV/script.js
+++ BT_S-Group/CV/script.js
@@ -24,6 +24,87 @@ document.getElementById('button2').onclick = () => {
     document.getElementById('blockbody-bg').style.animationName="showbg";
   }
 
+document.getElementById('i1c').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="none";
+  document.getElementById("i1b").classList.remove("active");
+}
+document.getElementById('i1a').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="block";
+  document.getElementById("i1b").classList.add("active");
+}
+
+document.getElementById('i2c').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="none";
+  document.getElementById("i2b").classList.remove("active");
+}
+document.getElementById('i2a').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="block";
+  document.getElementById("i2b").classList.add("active");
+}
+
+document.getElementById('i3c').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="none";
+  document.getElementById("i3b").classList.remove("active");
+}
+document.getElementById('i3a').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="block";
+  document.getElementById("i3b").classList.add("active");
+}
+
+document.getElementById('i4c').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="none";
+  document.getElementById("i4b").classList.remove("active");
+}
+document.getElementById('i4a').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="block";
+  document.getElementById("i4b").classList.add("active");
+}
+
+document.getElementById('i5c').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="none";
+  document.getElementById("i5b").classList.remove("active");
+}
+document.getElementById('i5a').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="block";
+  document.getElementById("i5b").classList.add("active");
+}
+
+document.getElementById('i6c').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="none";
+  document.getElementById("i6b").classList.remove("active");
+}
+document.getElementById('i6a').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="block";
+  document.getElementById("i6b").classList.add("active");
+}
+
+document.getElementById('i7c').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="none";
+  document.getElementById("i7b").classList.remove("active");
+}
+document.getElementById('i7a').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="block";
+  document.getElementById("i7b").classList.add("active");
+}
+
+document.getElementById('i8c').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="none";
+  document.getElementById("i8b").classList.remove("active");
+}
+document.getElementById('i8a').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="block";
+  document.getElementById("i8b").classList.add("active");
+}
+
+document.getElementById('i9c').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="none";
+  document.getElementById("i9b").classList.remove("active");
+}
+document.getElementById('i9a').onclick = () => {
+  document.getElementById('carouselExampleFade').style.display="block";
+  document.getElementById("i9b").classList.add("active");
+}
+
 function hideMenu() {
     document.getElementById("menu").classList.remove("open");
     document.getElementById("tab").classList.add("move1");
